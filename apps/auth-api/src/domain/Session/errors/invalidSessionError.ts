b59945a732627d67ac112fb95fee850c8903b84b
SnowDrive <luc.moralesr@gmail.com>
2023-09-03 08:45:53 -0300

    feat: add invalid issuer, issued at, expiration and audience error messages

--- apps/auth-api/src/domain/Session/errors/invalidSessionError.ts
+++ apps/auth-api/src/domain/Session/errors/invalidSessionError.ts
@@ -7,4 +7,8 @@ export class InvalidSessionError extends Error {
 
 export enum InvalidSessionErrorMessages {
 	INVALID_SUBJECT = 'Property "sub" is required and must be a string in UUID format',
+	INVALID_ISSUER = 'Property "iss" is required and must be a string with at least 3 characters and a maximum of 15',
+	INVALID_ISSUED_AT = 'Property "iat" is required and must be a positive integer',
+	INVALID_EXPIRATION = 'Property "exp" is required and must be a positive integer',
+	INVALID_AUDIENCE = 'Property "aud" is required and must be a string with at least 3 characters and a maximum of 15',
 }
